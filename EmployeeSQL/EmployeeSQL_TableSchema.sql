3bde1e638d2a29d93cc2bf2b483b3969bca98e33
Jamie Hutton <136202083+jnhutton@users.noreply.github.com>
2023-08-31 20:11:51 -0500

    Add files via upload
    adding files in

---
+++ EmployeeSQL/EmployeeSQL_TableSchema.sql
@@ -0,0 +1,58 @@
+CREATE TABLE departments
+(
+	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM departments;
+
+CREATE TABLE titles
+(
+	title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+	title VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM titles;
+
+CREATE TABLE employees
+(
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE dept_emp
+(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE dept_manager
+(
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_manager;
+
+CREATE TABLE salaries
+(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries;
